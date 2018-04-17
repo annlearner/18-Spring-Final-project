238afb906beba4e7f538d693dc9a1656b4eefaa2
jenniferadaint <35616520+jenniferadaint@users.noreply.github.com>
2018-04-17 15:21:42 -0700

    version0.0

--- src/com.neuSpring18/ui/DealerUI/DealerCommonFrame.java
+++ src/com.neuSpring18/ui/DealerUI/DealerCommonFrame.java
@@ -1,4 +1,27 @@
 package com.neuSpring18.ui.DealerUI;
 
-public class DealerCommonFrame {
+import javax.swing.*;
+
+public class DealerCommonFrame extends JFrame {
+
+    protected JButton addButton, goButton;
+
+    protected JTextField searchTextArea;
+    protected JTextField priceStart, priceEnd;
+    protected JTextField yearStart, yearEnd;
+
+    protected JLabel searchLabel;
+    protected JLabel filterLabel;
+    protected JLabel priceLabel, yearLabel, colorLabel, categoryLabel, typeLabel;
+
+    protected void makeItVisible(){
+
+        setSize(800,600);
+        setVisible(true);
+        setResizable(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+    }
+
 }
