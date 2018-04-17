dac0761b1b63e5054f1e83205ff44b8eb235a73c
wonderfullee <35618935+wonderfullee@users.noreply.github.com>
2018-04-17 16:02:31 -0700

    Update Sorting.java

--- src/com.neuSpring18/dto/Sorting.java
+++ src/com.neuSpring18/dto/Sorting.java
@@ -1,5 +1,5 @@
 package com.neuSpring18.dto;
 
 public enum Sorting {
-    ASCEND_PRICE, DESCEND_PRICE, ASCEND_YEAR, DESCEND_YEAR ,Default
+    ASCEND_PRICE, DESCEND_PRICE, ASCEND_YEAR, DESCEND_YEAR, Default
 }
