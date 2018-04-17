0de8df55c10f53c5e3c91d093ea0ded3cf6c84d9
wonderfullee <35618935+wonderfullee@users.noreply.github.com>
2018-04-16 20:17:35 -0700

    Update DealerService.java

--- src/com.neuSpring18/service/DealerService.java
+++ src/com.neuSpring18/service/DealerService.java
@@ -5,5 +5,5 @@ import com.neuSpring18.dto.Dealer;
 import java.util.Collection;
 
 public interface DealerService {
-    Collection<Dealer> getAllDealers();
+    Collection<Dealer> findAllDealers();
 }
