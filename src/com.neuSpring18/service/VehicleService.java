30b17514731fd4b9d72830cdf64bf4ef02472a01
wonderfullee <35618935+wonderfullee@users.noreply.github.com>
2018-04-16 20:16:23 -0700

    Update VehicleService.java

--- src/com.neuSpring18/service/VehicleService.java
+++ src/com.neuSpring18/service/VehicleService.java
@@ -5,8 +5,8 @@ import com.neuSpring18.dto.*;
 import java.util.Collection;
 
 public interface VehicleService {
-    Inventory getVehiclesByFilter(String dealerID, Filter filter, Sorting sorting, Paging paging);
-    Inventory getVehiclesByDealer(String dealerID);
+    Inventory findVehiclesByFilter(String dealerID, Filter filter, Sorting sorting, Paging paging);
+    Inventory findVehiclesByDealer(String dealerID);
     String addVehicle(String dealerID, Vehicle v); // Return the ID of the new vehicle
     boolean editVehicle(String dealerID, Vehicle v); // Return true if edit successfully. Notice that we might add some error control later.
     boolean removeVehicle(String dealerID, String vehicleID); // Might add error control as well.
