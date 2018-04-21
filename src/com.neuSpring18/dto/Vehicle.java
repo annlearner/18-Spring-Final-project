b27debedfedb054813f8c9fd44121436f5914424
Luqing Jing <jingluqing@gmail.com>
2018-04-21 14:18:06 -0700

    modify searchFilter and simplify codes

--- src/com.neuSpring18/dto/Vehicle.java
+++ src/com.neuSpring18/dto/Vehicle.java
@@ -139,14 +139,14 @@ public class Vehicle {
     public String toSearchString() {
         StringBuilder sb = new StringBuilder();
         sb.append(id).append("~");
-        sb.append(webId).append("~");
+//        sb.append(webId).append("~");
         sb.append(category).append("~");
         sb.append(year).append("~");
         sb.append(make).append("~");
         sb.append(model).append("~");
         sb.append(trim).append("~");
-        sb.append(bodyType).append("~");
-        sb.append(price);
+        sb.append(bodyType);
+//        sb.append(price);
         return sb.toString();
     }
 }
