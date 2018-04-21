71843e51ae38ae7d2914547b41f454aff0979745
Zane Wang <363177773@qq.com>
2018-04-20 23:31:12 -0700

    Make the sorting more elegant

--- src/com.neuSpring18/dto/Sorting.java
+++ src/com.neuSpring18/dto/Sorting.java
@@ -1,5 +1,37 @@
 package com.neuSpring18.dto;
 
+import java.util.Comparator;
+
 public enum Sorting {
-    ASCEND_PRICE, DESCEND_PRICE, ASCEND_YEAR, DESCEND_YEAR, DEFAULT
+
+//    Here we use the Comparator.comparingDouble(Vehicle::getPrice()) method, which is short for:
+//    Comparator.comparingDouble((Vehicle v) -> v.getPrice());
+//    which is short for:
+//    (a, b) -> { return Double.compare(a.getPrice(), b.getPrice()); }
+//    which is short for:
+//    class Sorter implements Comparator<Vehicle> {
+//        @Override
+//        public int compare(Vehicle a, Vehicle b) {
+//            return Double.compare(a.getPrice(), b.getPrice());
+//        }
+//    }
+//    which is extremely elegant!!!
+
+    ASCEND_PRICE(Comparator.comparingDouble(Vehicle::getPrice)),
+    DESCEND_PRICE(Comparator.comparingDouble(Vehicle::getPrice).reversed()),
+    ASCEND_YEAR(Comparator.comparingInt(Vehicle::getYear)),
+    DESCEND_YEAR(Comparator.comparingInt(Vehicle::getYear).reversed()),
+    DEFAULT(Comparator.comparing(Vehicle::getId));
+
+    private final Comparator<Vehicle> sorter;
+
+    Sorting(Comparator<Vehicle> sorter) {
+        this.sorter = sorter;
+    }
+
+    public Comparator<Vehicle> getSorter() {
+        return sorter;
+    }
+
+
 }
