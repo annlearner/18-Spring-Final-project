88fb2e77dd24bd564f2ef3bcda219fbd3f572ad6
yuetang95 <tang.yue@husky.neu.edu>
2018-04-23 18:10:56 -0700

    update generateID function and add function; fixed bug in add function. updated April 23th.

--- src/com.neuSpring18/io/UserIO.java
+++ src/com.neuSpring18/io/UserIO.java
@@ -1,8 +1,10 @@
 package com.neuSpring18.io;
 
 import java.io.*;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,17 +23,11 @@ public class UserIO implements UserIOInterface {
             String[] splited;
             while ((read = br.readLine()) != null) {
                 if (mode.equals("ID")) {
-
-                    if(filename.equals("dealers")) {
-                        splited = read.split("\t");
-                    }else {
-                        splited=read.split("~");
-                    }
-                    if (splited.length > 0)
-                        result.add(splited[0]);
+                    if(filename.equals("dealers")) splited = read.split("\t");
+                    else splited=read.split("~");
+                    if (splited.length > 0) result.add(splited[0]);
                 } else if (mode.equals("All"))
-                    result.add(read);
-            }
+                    result.add(read); }
         }catch(Exception e){
             e.printStackTrace();
         }finally {
@@ -44,21 +40,20 @@ public class UserIO implements UserIOInterface {
     @Override
     public String addVehicleToDealer(String dealerID, String vehicleString) {
         String selected=path+dealerID;
-        String newID=null;
-        //hasID no? then generate Id
-        if(!hasId(dealerID,newID)) {
-            newID = generateId();
+        File file=new File(selected);
+        String newID;
+        while(true){
+           newID=generateId();
+           if(!hasId(dealerID,newID))  break;
         }
         //add the info to file
         FileWriter fw=null;
         try {
-            fw=new FileWriter(selected,true);
+            fw=new FileWriter(file,true);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if (fw != null) {
-            bw=new BufferedWriter(fw);
-        }
+        if (fw != null) bw=new BufferedWriter(fw);
         if (bw != null) {
             try (PrintWriter pw = new PrintWriter(bw)) {
                 pw.println(newID+vehicleString);
@@ -70,12 +65,26 @@ public class UserIO implements UserIOInterface {
     }
 
     /**
-     * Generate a 10 digit ID randomly
+     * Generate an ID randomly by using UUID
      * @return the ID
      */
+
     private String generateId() {
-        long number=(long)Math.floor(Math.random()*9_000_000_000L)+1_000_000_000L;
-        return Long.toString(number);
+        //generate a 37 digit ID
+        return String.format("%d",new BigInteger(UUID.randomUUID().toString().replace("-",""),16));
+
+
+        /*
+        // return a 10 digit ID
+        int x=0;
+		String ran=String.format("%d",new BigInteger(UUID.randomUUID().toString().replace("-",""),16));
+		while(true) {
+		x=(int) (Math.random()*ran.length());
+		if(x+10<ran.length()-1) break;
+		}
+        return ran.substring(x,x+10));
+        */
+
     }
 
     private boolean hasId(String filename,String ID) {
@@ -99,7 +108,7 @@ public class UserIO implements UserIOInterface {
                     Matcher matcher=pattern.matcher(line) ;
                     line=matcher.replaceAll(vehicleString);
                 }
-                wholeline+=line+System.lineSeparator();
+                wholeline=line+System.lineSeparator();
             }
             fw=new FileWriter(file);
             fw.write(wholeline);
